0d9e4418aecc7c583c0c4e0d020895f758f24bab
Tejas <hntejas@gmail.com>
2021-07-18 11:25:37 +0530

    feat: implementing push, pop, shift and unshift methods for doubly linked list

---
+++ doubly-linked-list/index.js
@@ -0,0 +1,82 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+
+    var removedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+      removedNode.prev = null;
+    }
+
+    this.length--;
+    return removedNode;
+  }
+
+  shift() {
+    if (!this.head) {
+      return undefined;
+    }
+
+    var removedNode = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.next;
+      this.head.prev = null;
+      removedNode.next = null;
+    }
+
+    this.length--;
+    return removedNode;
+  }
+
+  unshift(val) {
+    var newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+
+    this.length++;
+    return newNode;
+  }
+}
---
+++ doubly-linked-list/readme.md
@@ -0,0 +1,26 @@
+# Doubly Linked List
+
+- Doubly linked list is a data structure which contains a list of nodes which are connected in series.
+- Each node will have it's value and reference/pointer to the next node and previous node.
+- It shall have properties like `head`, `tail` and `length`.
+- Head refers to the first node, tail refers to the last node and length refers to the total nodes in the structure.
+- Unlike arrays, this DS does not support indexes i.e., you cannot access a particular node directly (Random access is not allowed).
+- To access an item, the list has to traversed until we reach the item.
+
+## Methods
+
+- Push
+
+  - Allows to add new nodes into the list.
+  - Returns the list.
+  - If there is a single node, `head` and `tail` shall point to the same node.
+  - If there is are no nodes, `head` and `tail` shall be `null`.
+  - When a new node is added, the `tail` shall point to the latest added node.
+
+- Pop
+
+  - Removes the last node from the list.
+  - Returns the last node which was removed.
+  - `tail` is reset to the last node after removal.
+  - If all the nodes are removed, `head` and `tail` will be set to `null`.
+  - On every removal, length is decremented accordingly.
